b473516f35a165e8ca8af256ace1565a986a8189
Eko Pramono <eko.pramono@gmail.com>
2020-01-17 01:02:40 +0700

    Add files via upload

---
+++ Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestRestAPI3.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpiryDateTime { get; set; }
+        public int PercentCompleted { get; set; }
+    }
+}
